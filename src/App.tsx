01f3cfbddcbfe59557220939dc3bf3bfbe0ac0d4
Marid <maridshikhkerimov@gmail.com>
2021-07-26 08:38:21 +0300

    2. Основные методы взаимодействия с localstorage. JSON stringify, parse.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState<number>(0)
+
+    let incHandler = () => {
+        setValue(value + 1)
+    }
+
+    const setToLocalStorageHandler = () => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+        localStorage.setItem('counterValue + 1', JSON.stringify(value + 1))
+    }
+
+    const getFromLocalStorage = () => {
+        let valueAsString = localStorage.getItem('counterValue')
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setValue(newValue)
+        }
+    }
+
+    const clearLocalStorage = () => {
+        localStorage.clear()
+        setValue(0)
+    }
+
+    const removeItemFromLocalStorage = () => {
+        localStorage.removeItem('counterValue' + 1)
+        setValue(0)
+    }
+
+    return (
+        <div className="App">
+            <h1>{value}</h1>
+            <button onClick={incHandler}>inc</button>
+            <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
+            <button onClick={getFromLocalStorage}>getFromLocalStorage</button>
+            <button onClick={clearLocalStorage}>clearLocalStorage</button>
+            <button onClick={removeItemFromLocalStorage}>removeItemFromLocalStorage</button>
+        </div>
+    );
 }
 
 export default App;
